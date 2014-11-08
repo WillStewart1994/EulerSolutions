1393f87c6e8b8ee15d74716c0d7c96f31a1cdb05
WillStewart1994 <Will.stewart1994@gmail.com>
2014-11-08 18:43:06 -0500

    Problem 15
    The combinatorics-based grid problem

--- Problem15.py
+++ Problem15.py
@@ -1,19 +1,22 @@
 #Recursive solution:
-def f(x, y):
-    if x == 1 and y == 1:
-        return 1
-    elif x <= 0:
-        return f(x, y-1)
-    elif y <= 0:
-        return f(x-1, y)
-    else:
-        return f(x-1, y) + f(x, y-1)
+table = {}
 
-print f(20)
+def f(x, y):
+    if (x, y) in table:
+        return table[(x, y)]
+    else:
+        if x <= 0 or y <= 0:
+            table[(x, y)] = 1
+            return 1
+        else:
+            result = f(x-1, y) + f(x, y-1)
+            table[(x, y)] = result
+            return result
+
+print f(20, 20)
 
 #There is also a closed form soluton. This is a very common problem in combinatorics.
 #There are a total of (x+y) directions to be travelled, and we must strictly the define
 #the position of either x or y of the directions. In combinatorics, this is defined as
 #choose(x+y, x) == choose(x+y, y) == (x+y)*(x+y-1)*...*(y+1)/(x!) == (x+y)*(x+y-1)*...*(x+1)/(y!)
-
 
